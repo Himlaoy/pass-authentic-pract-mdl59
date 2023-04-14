7223fda5f8f2e4418b15d24d9c97ed70b1ab3ec0
himaloyash <himaloyashis98@gmail.com>
2023-04-14 22:25:05 +0600

    log success

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../../firebase.config';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const auth = getAuth(app)
 const Login = () => {
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
 
 
 
     const handleLogin=(event)=>{
+
         event.preventDefault()
         const email = event.target.email.value
         const password = event.target.password.value
         console.log(email, password)
 
+        if(!/(?=.*[A-Z])/.test(password)){
+            setError('please put uppercase latter')
+            return
+        }
+        else if(!/(?=.*[a-z])/.test(password)){
+            setError('please put lowercase latter')
+            return
+        }
+        else if(!password.length<6){
+            setError('pass code will be at least 6 character')
+        }
+
         signInWithEmailAndPassword(auth, email, password)
         .then(result=>{
-            // const loggedUser = result.user
-            console.log(result)
+            const loggedUser = result.user
+            console.log(loggedUser)
+            event.target.reset()
+            setSuccess('logged in successfully')
+            setError('')
         })
         .catch(error=>{
             console.log(error.message)
+            setSuccess('')
+            setError(error.message)
+        
         })
 
 
@@ -39,6 +61,8 @@ const Login = () => {
                 <input type="submit" className='mb-3 rounded' value="Login" />
             </form>
             <p>Already haven't a account? please <Link to="/register">Register</Link></p>
+            <p className='text-success'>{success}</p>
+            <p className='text-danger'>{error}</p>
         </div>
     );
 };
