d520387c76d3f2e701cafc37068a0022d98e1628
himaloyash <himaloyashis98@gmail.com>
2023-04-14 21:48:21 +0600

    signin form almost done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+
 
 
 const Login = () => {
+
+
+
+    const handleLogin=(event)=>{
+        event.preventDefault()
+        console.log(event.target.email)
+    }
+
     return (
-        <form className=''>
-            <input type="email" name="email" id="email" placeholder='enter your email'/>
-            <br />
-            <input type="password" name="password" id="password" placeholder='enter your password'/>
-            <br />
-            <input type="submit" value="Login" />
-        </form>
+
+        <div className='mx-auto'>
+            <h2>Please Login</h2>
+            <form onSubmit={handleLogin} className='mx-auto'>
+                <input type="email" className='mb-3 rounded' name="email" id="email" placeholder='enter your email' />
+                <br />
+                <input type="password" className='mb-3 rounded' name="password" id="password" placeholder='enter your password' />
+                <br />
+                <input type="submit" className='mb-3 rounded' value="Login" />
+            </form>
+        </div>
     );
 };
 
