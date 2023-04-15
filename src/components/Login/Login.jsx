78549f4f325f3cbcc25299aae6bb567038fac996
himaloyash <himaloyashis98@gmail.com>
2023-04-15 12:38:22 +0600

    project done now I'm go another one

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { getAuth, sendEmailVerification, signInWithEmailAndPassword } from 'firebase/auth';
+import React, {  useState } from 'react';
+import { getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import app from '../../firebase.config';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +9,7 @@ const auth = getAuth(app)
 const Login = () => {
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
+    
 
 
 
@@ -35,6 +36,7 @@ const Login = () => {
         .then(result=>{
             const loggedUser = result.user
             console.log(loggedUser)
+            
             event.target.reset()
             
             setSuccess('logged in successfully')
@@ -48,17 +50,35 @@ const Login = () => {
         })
 
 
-        
-
-
     }
+
+
+    const resetPass =()=>{
+        const email = emailRef.current.value
+       if(!email){
+        alert('please enter a valid email')
+       }
+       sendPasswordResetEmail(auth, email)
+       .then(result=>{
+        alert('please check your email')
+        console.log(result)
+       })
+       .catch(error=>{
+        setError(error.message)
+       })
+    }
+
+
+   
 
     return (
 
         <div className='mx-auto'>
             <h2>Please Login</h2>
             <form onSubmit={handleLogin} className='mx-auto'>
-                <input type="email" className='mb-3 rounded' name="email" id="email" placeholder='enter your email' />
+               
+
+                <input type="email" className='mb-3 rounded' ref={emailRef} name="email" id="email" placeholder='enter your email' />
                 <br />
                 <input type="password" className='mb-3 rounded' name="password" id="password" placeholder='enter your password' />
                 <br />
@@ -67,6 +87,7 @@ const Login = () => {
             <p>Already haven't a account? please <Link to="/register">Register</Link></p>
             <p className='text-success'>{success}</p>
             <p className='text-danger'>{error}</p>
+            <p>Forgot password? please <button onClick={resetPass} className='btn btn-link'>Reset Password</button></p>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {createUserWithEmailAndPassword, getAuth, sendEmailVerification} from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth'
 import app from '../../firebase.config';
 import { Link } from 'react-router-dom';
 
@@ -9,54 +9,71 @@ const auth = getAuth(app)
 const Register = () => {
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
+    const emailRef = useRef()
 
 
-    const handleSubmit =(event)=>{
+    const handleSubmit = (event) => {
         event.preventDefault()
         const email = event.target.email.value
-        const password =event.target.password.value
+        const password = event.target.password.value
+        const name = event.target.name.value
         console.log(email, password)
 
-        if(!/(?=.*[a-z])/.test(password)){
+        if (!/(?=.*[a-z])/.test(password)) {
             setError('Please use lowercase letter')
             return
         }
-        else if(!/(?=.*[A-Z])/.test(password)){
+        else if (!/(?=.*[A-Z])/.test(password)) {
             setError('please use uppercase latter')
             return
         }
-        else if(password.length<6){
+        else if (password.length < 6) {
             setError('pass code will be at least 6 character')
             return
         }
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result=>{
-            const loggedUser = result.user
-            console.log(loggedUser)
-            sendEmailVerify(loggedUser)
-            setSuccess('log in successfully')
-            event.target.reset()
-            setError('')
-        })
+            .then(result => {
+                const loggedUser = result.user
+                console.log(loggedUser)
+                userUpdate(loggedUser,name)
 
-     .catch(error=>{
-        console.log(error)
-        setError(error.message)
-        setSuccess('')
-     })
+                sendEmailVerify(loggedUser)
+                setSuccess('log in successfully')
+                event.target.reset()
+                setError('')
+            })
+
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+                setSuccess('')
+            })
 
 
-     const sendEmailVerify = (user)=>{
-        sendEmailVerification(user)
-        .then(result=>{
-            const loggedUser = result.user
-            console.log(loggedUser)
-            alert('check your email')
-        })
-        
+        const sendEmailVerify = (user) => {
+            sendEmailVerification(user)
+                .then(result => {
+                    console.log(result)
+                    alert('check your email')
+                })
+
+        }
+
     }
-        
+
+
+    const userUpdate =(user, name)=>{
+        updateProfile(user,{
+            displayName:name
+        })
+        .then(()=>{
+            console.log('updated successfully')
+        })
+        .catch(error=>{
+            console.log(error.message)
+        })
+
     }
 
 
@@ -66,7 +83,10 @@ const Register = () => {
                 <h2>Register</h2>
             </div>
             <form onSubmit={handleSubmit}>
-                <input type="email" className='mb-3 rounded p-1'  name="email" required id="email" placeholder='your email' />
+                <input type="name" className='mb-3 rounded' ref={emailRef} name="name" id="name" placeholder='enter your name' />
+                <br />
+
+                <input type="email" className='mb-3 rounded p-1' name="email" required id="email" placeholder='your email' />
                 <br />
                 <input type="password" className='mb-3 rounded p-1' name="password" required id="password" placeholder='your password' />
                 <br />
